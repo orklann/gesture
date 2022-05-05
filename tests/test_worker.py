e0c785156babf641dbc7006f4a95cad3f3c08b92
Aaron Elkins <threcius@yahoo.com>
2022-05-05 10:54:12 +0800

    Update test case

--- tests/test_worker.py
+++ tests/test_worker.py
@@ -3,6 +3,6 @@ from gesture.worker import Worker
 
 class TestWorker(unittest.TestCase):
     def test_start(self):
-        job_str = '{"class_name": "DummyJob", "args": [1, 2, 3], "at": 12345}'
+        job_str = '{"class": "DummyJob", "args": [1, 2, 3], "at": 12345}'
         worker = Worker()
         worker.run(job_str)
