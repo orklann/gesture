90030dfabfeafda75cc301a87f1f6daf06905616
Aaron Elkins <threcius@yahoo.com>
2022-05-07 13:27:35 +0800

    Update comment

--- gesture/utils.py
+++ gesture/utils.py
@@ -1,7 +1,7 @@
 import time
 
 def normalize_item(item):
-    # `created_at` is the key to make two same jobs different, so normally
+    # `created_at` is the key to make two same jobs different, so in general
     # we have two same jobs in Redis.
     item["created_at"] = time.time()
     return item
