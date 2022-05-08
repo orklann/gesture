5f2e77262e4fdbd83ead1ab727d3dd8a298d74ae
Aaron Elkins <threcius@yahoo.com>
2022-05-08 09:53:26 +0800

    Show banner in cli

--- gesture/cli.py
+++ gesture/cli.py
@@ -7,6 +7,26 @@ import sys
 import click
 from .server import Server
 
+def show_banner():
+    banner = """
+                                      ,----,                               
+                                    ,/   .`|                               
+  ,----..      ,---,. .--.--.     ,`   .'  :          ,-.----.      ,---,. 
+ /   /   \   ,'  .' |/  /    '. ;    ;     /      ,--,\    /  \   ,'  .' | 
+|   :     :,---.'   |  :  /`. .'___,/    ,'     ,'_ /|;   :    \,---.'   | 
+.   |  ;. /|   |   .;  |  |--`|    :     | .--. |  | :|   | .\ :|   |   .' 
+.   ; /--` :   :  |-|  :  ;_  ;    |.';  ,'_ /| :  . |.   : |: |:   :  |-, 
+;   | ;  __:   |  ;/|\  \    ``----'  |  |  ' | |  . .|   |  \ ::   |  ;/| 
+|   : |.' .|   :   .' `----.   \  '   :  |  | ' |  | ||   : .  /|   :   .' 
+.   | '_.' |   |  |-, __ \  \  |  |   |  :  | | :  ' ;;   | |  \|   |  |-, 
+'   ; : \  '   :  ;/|/  /`--'  /  '   :  |  ; ' |  | '|   | ;\  '   :  ;/| 
+'   | '/  .|   |    '--'.     /   ;   |.':  | : ;  ; |:   ' | \.|   |    \ 
+|   :    / |   :   .' `--'---'    '---'  '  :  `--'   :   : :-' |   :   .' 
+ \   \ .'  |   | ,'                      :  ,      .-.|   |.'   |   | ,'   
+  `---`    `----'                         `--`----'   `---'     `----'     
+    """
+    print(banner)
+
 def import_script(script):
     if not script:
         print("Please specify a script file by using -s option. Exiting...")
@@ -19,11 +39,12 @@ def import_script(script):
     base_name = os.path.basename(full_path)
     module_name = os.path.splitext(base_name)[0]
     __import__(module_name)
-    print("Imported", full_path)
+    print("Imported script:", full_path)
 
 @click.command()
 @click.option('-s', '--script', help='Location of python file to import')
 def main(script):
+    show_banner()
     import_script(script)
     server = Server()
     server.start()
