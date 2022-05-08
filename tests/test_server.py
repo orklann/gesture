99dd1b844ecec2542da9c523a159395bd82a651c
Aaron Elkins <threcius@yahoo.com>
2022-05-08 09:41:44 +0800

    Update test case

--- tests/test_server.py
+++ tests/test_server.py
@@ -1,3 +1,4 @@
+import json
 import unittest
 from gesture.server import Server
 from .dummy import DummyJob
@@ -5,7 +6,14 @@ from .utils import clear_set
 
 class TestServer(unittest.TestCase):
     def test_fetch(self):
-        pass
+        server = Server()
+        # clear all jobs before testing
+        clear_set(server.redis, "schedule")
+        DummyJob.perform_async(100)
+        DummyJob.perform_async(200)
+        job_str = server.fetch()
+        job_dict = json.loads(job_str)
+        self.assertEqual(job_dict["args"], [100])
 
     def test_start(self):
         pass
