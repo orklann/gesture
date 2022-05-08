91160f2de95db07dbdb0fec44a6b651a952e92f3
Aaron Elkins <threcius@yahoo.com>
2022-05-08 14:28:34 +0800

    Exit instead of return from import_script()

--- gesture/cli.py
+++ gesture/cli.py
@@ -30,11 +30,11 @@ def show_banner():
 def import_script(script):
     if not script:
         print("Please specify a script file by using -s option. Exiting...")
-        return
+        sys.exit(1)
     full_path = os.path.abspath(script)
     if not os.path.exists(full_path):
         print('"%s" not found, exiting...' % full_path)
-        return
+        sys.exit(1)
     sys.path.append(os.path.dirname(full_path))
     base_name = os.path.basename(full_path)
     module_name = os.path.splitext(base_name)[0]
